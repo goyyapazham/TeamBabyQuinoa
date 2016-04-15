9ffc3744527a5399f59bbdffbcc75b4c4b440f64
linrichard99 <rlin5@stuy.edu>
2016-04-14 22:54:55 -0400

    activity 5

--- pixLab/classes/Picture.java
+++ pixLab/classes/Picture.java
@@ -97,6 +97,53 @@ public class Picture extends SimplePicture
       }
     }
   }
+
+    /** Method to only keep blue */
+  public void keepOnlyBlue()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        pixelObj.setRed(0);
+	pixelObj.setGreen(0);
+      }
+    }
+  }
+
+    /** Method to only keep blue */
+  public void negate()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+	  pixelObj.setRed(255-(pixelObj.getRed()));
+	  pixelObj.setGreen(255-(pixelObj.getGreen()));
+	  pixelObj.setBlue(255-(pixelObj.getBlue()));
+      }
+    }
+  }
+
+    /** Method to only keep blue */
+  public void grayscale()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+	  int pixAve = ((pixelObj.getRed()) + (pixelObj.getGreen()) + (pixelObj.getBlue())) / 3;
+	  pixelObj.setRed(pixAve);
+	  pixelObj.setGreen(pixAve);
+	  pixelObj.setBlue(pixAve);
+      }
+    }
+  }
+
+    
   
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
